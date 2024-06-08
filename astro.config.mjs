4950d31e1d57e79f77de9083271f5994fa77a479
Nick Chen <1358390935@qq.com>
2024-06-08 21:18:44 +0800

    feat: change `/downloads` into dynamic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
-  output: 'static'
+  output: 'hybrid',
+  adapter: node({
+    mode: "standalone"
+  })
 });
