defacd7f4c0a0551b9a90db172045657f639da50
Nick Chen <1358390935@qq.com>
2024-06-10 10:01:55 +0800

    SEO: optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()],
+  site: 'https://myani.org',
+  integrations: [vue(), tailwind(), sitemap()],
   output: 'static'
 });
