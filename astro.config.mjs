7343df2de8604892dd56bb3e87b7a794d14a6b13
Nick Chen <119087246+nick-cjyx9@users.noreply.github.com>
2024-06-23 11:47:45 +0800

    chore: enable prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: 'https://myani.org',
   integrations: [vue(), tailwind(), sitemap()],
-  output: 'static'
+  output: 'static',
+  prefetch: true
 });
