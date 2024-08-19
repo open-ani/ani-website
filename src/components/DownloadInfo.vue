1bae4fd22b3b938aa2fe54096307fe6a895094a1
Nick Chen <119087246+nick-cjyx9@users.noreply.github.com>
2024-08-20 07:48:58 +0800

    fix: revert patch

--- src/components/DownloadInfo.vue
+++ src/components/DownloadInfo.vue
@@ -1,6 +1,6 @@
 <template>
     <ul v-if="loaded === 1&&outputs" class="py-3">
-        <li v-for="(k,v) in nameMap" aria-label="win" class="p-4 flex">
+        <li v-for="(k, v) in nameMap" aria-label="win" class="p-4 flex">
             <div class="w-full flex">
                 <svg class="w-12 h-12 p-1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" /><path fill="currentColor" d="M8 5v2H5v13h14V7h-3V5h3a2 2 0 0 1 2 2v13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2zm4-3a1 1 0 0 1 1 1v10.828L14.828 12a1 1 0 1 1 1.415 1.414l-3.36 3.359a1.25 1.25 0 0 1-1.767 0l-3.359-3.359A1 1 0 1 1 9.172 12L11 13.828V3a1 1 0 0 1 1-1" /></g></svg>
                 <div class="ml-2 w-fit flex flex-col">
@@ -11,7 +11,7 @@
             <div class="space-x-2 flex text-nowrap my-auto w-fit float-end">
                 <a :href="outputs.downloadUrlAlternativesMap[v][0]" class="hover:bg-slate-600 border-2 border-white rounded px-3 py-2 text-sm text-slate-300">主线</a>
                 <a :href="outputs.downloadUrlAlternativesMap[v][1]" class="hover:bg-slate-600 border-2 border-white rounded px-3 py-2 text-sm text-slate-300">备线</a>
-                <button class="hover:bg-slate-600 border-2 border-white rounded px-3 py-2 text-sm text-slate-300" v-if="v === 'android'&& isPC" @click="showQR=!showQR">扫码下载</button>
+                <button class="hover:bg-slate-600 border-2 border-white rounded px-3 py-2 text-sm text-slate-300" v-if="v === 'android-universal'&& isPC" @click="showQR=!showQR">扫码下载</button>
             </div>
             <!-- TODO: sort to pin the correct device type -->
         </li>
@@ -51,7 +51,7 @@ interface responseType {
         'macos-aarch64': string[],
         'macos-x86_64': string[],
         'windows-x86_64': string[],
-        'android': string[]
+        'android-universal': string[]
     },
     publishTime: number,
     qrcodeUrls: string[]
@@ -61,7 +61,7 @@ const nameMap = ref({
     'macos-aarch64': 'macOS aarch64 (M 系列芯片)',
     'macos-x86_64': 'macOS x86_64 (Intel 芯片)',
     'windows-x86_64': 'Windows X86_64',
-    'android': '安卓 APK'
+    'android-universal': '安卓 APK'
 })
 
 const outputs:Ref<responseType|undefined> = ref()
