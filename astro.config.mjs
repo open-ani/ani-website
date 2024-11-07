0d50ae17d0d6ef98e5154a0e7fa23d34d97ee6c1
目棃 <BT-Muli@outlook.com>
2024-11-07 10:26:56 +0800

    🐛 路由重定向
    close #11

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
 
-import { defineConfig } from 'astro/config'
+import {defineConfig} from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://myani.org',
-  integrations: [vue(), tailwind(), sitemap()],
-  output: 'static',
-  prefetch: true,
+    site: 'https://myani.org',
+    integrations: [vue(), tailwind(), sitemap()],
+    output: 'static',
+    prefetch: true,
+    redirects: {
+        '/downloads/[...slug]': '/downloads',
+    }
 })
