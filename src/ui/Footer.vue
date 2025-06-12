daa48e8309242916d38d4b39818d59458ec66986
Nick Chen <i@nickchen.top>
2025-06-12 10:11:02 +0800

    chore: fix footer style

--- src/ui/Footer.vue
+++ src/ui/Footer.vue
@@ -1,25 +1,12 @@
 <template>
   <div
-    class="text-sm/6
-      mx-auto
-      flex
-      flex-wrap
-      items-center
-      text-slate-400
-      border-t-[1px]
-      border-slate-100
-       w-full
-       xl:w-3/5
-       px-6
-       h-[3.4rem]"
+    class="text-sm leading-6 mx-auto flex items-center text-slate-400 border-t border-slate-100 w-full xl:w-3/5 px-6 h-14"
   >
-    <p class="w-full flex gap-1">
-      <span>© 2024 ~ 2025</span>
-      <a class="text-blue-400" target="_blank" href="https://github.com/open-ani/animeko">Animeko</a>
-      <span>❤ Website by </span>
-      <a href="https://github.com/open-ani/ani-website" target="_blank" class=" text-blue-400">Nick</a>
-    </p>
-    <div class="inline space-x-2 w-fit text-nowrap ml-auto">
+    <div class="float-left">
+      <span>© 2024-2025 </span>
+      <a class="text-blue-400" target="_blank" href="https://github.com/open-ani/animeko">❤ Animeko</a>
+    </div>
+    <div class="space-x-2 w-fit ml-auto whitespace-nowrap">
       <a
         href="https://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=2EbZ0Qxe-fI_AHJLCMnSIOnqw-nfrFH5&authKey=L31zTMwfbMG0FhIgt8xNHGOFPHc531mSw2YzUVupHLRJ4L2f8xerAd%2ByNl4OigRK&noverify=0&group_code=927170241"
         class="underline"
