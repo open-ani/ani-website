90a3ff0d3f2635bdcfb6a561aec2dc9551d4cf62
StageGuard <sg@mamoe.net>
2026-06-16 17:46:58 +0800

    feat: add second home ad banner

--- src/config/ads.ts
+++ src/config/ads.ts
@@ -1,3 +1,20 @@
 export const homeAdConfig = {
-  href: "https://aplsof2fd.flirnapolfwemvrybhszorex.com/ky/53673/ifkvzdsj",
+  ads: [
+    {
+      id: "home-admedia-1",
+      href: "https://aplsof2fd.flirnapolfwemvrybhszorex.com/ky/53673/ifkvzdsj",
+      imageSrc: "/images/admedia/1.png",
+      alt: "广告",
+      width: 1200,
+      height: 160,
+    },
+    {
+      id: "home-admedia-2",
+      href: "https://www.aikda.com/?rf=2018d0a9",
+      imageSrc: "/images/admedia/2.gif",
+      alt: "广告",
+      width: 1280,
+      height: 226,
+    },
+  ],
 } as const;
